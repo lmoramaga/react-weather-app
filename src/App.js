041effa1664dc8c1da439fd3fc63f5a56bc0f117
lmoramaga <linahmoramaga@gmail.com>
2026-07-16 13:30:22 +0200

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App </h1>
+
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/lmoramaga" target="_blank" rel="noreferrer">
+          Lina Moramaga
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/lmoramaga/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
