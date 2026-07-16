a3479ff1c19f175bbde3596db135d578172fc573
lmoramaga <linahmoramaga@gmail.com>
2026-07-16 14:07:45 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App </h1>
+      <div className="container">
+        <h1>Weather App </h1>
 
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/lmoramaga" target="_blank" rel="noreferrer">
-          Lina Moramaga
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/lmoramaga/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/lmoramaga"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Lina Moramaga
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/lmoramaga/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
