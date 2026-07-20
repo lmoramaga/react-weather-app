92fcbc13f5d2d1acfd1a744167c008cd1d88a487
lmoramaga <linahmoramaga@gmail.com>
2026-07-20 11:05:30 +0200

    Displayed weather forecast of one day

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -24,7 +24,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#1e1e1e"
-      size={42}
+      size={props.size}
       animate={true}
     />
   );
